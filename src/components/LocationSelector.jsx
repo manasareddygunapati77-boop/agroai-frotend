889f866d591540f94fe0829825910e4a678b6f41
manasareddygunapati77-boop <manasareddygunapati77@gmail.com>
2026-06-22 09:37:35 +0530

    Add files via upload

--- src/components/LocationSelector.jsx
+++ src/components/LocationSelector.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../styles/LocationSelector.css";
+import "../styles/Locationselector.css";
 
 function LocationSelector({ onLocationChange }) {
   const [location, setLocation] = useState(null);
